6ba6bf851b1b07b01d8d40fcb61b463850761a6d
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-12-18 20:15:38 +0300

    Examples test update

--- src/test/java/com/epam/reportportal/jbehave/ExamplesTest.java
+++ src/test/java/com/epam/reportportal/jbehave/ExamplesTest.java
@@ -4,39 +4,51 @@
 
 package com.epam.reportportal.jbehave;
 
+import com.epam.reportportal.jbehave.integration.feature.StockExamplesSteps;
 import com.epam.reportportal.jbehave.utils.TestUtils;
 import com.epam.reportportal.service.ReportPortal;
 import com.epam.reportportal.service.ReportPortalClient;
 import com.epam.reportportal.util.test.CommonUtils;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class ExamplesTest {
 
 	private final String storyId = CommonUtils.namedId("story_");
 	private final String scenarioId = CommonUtils.namedId("scenario_");
-	private final List<String> stepIds = Stream.generate(() -> CommonUtils.namedId("step_")).limit(5).collect(Collectors.toList());
+	private final List<String> exampleIds = Stream.generate(() -> CommonUtils.namedId("example_")).limit(2).collect(Collectors.toList());
+
+	private final List<Pair<String, String>> stepIds = exampleIds.stream()
+			.flatMap(e -> Stream.generate(() -> Pair.of(e, CommonUtils.namedId("step_"))).limit(3))
+			.collect(Collectors.toList());
 
 	private final ReportPortalClient client = mock(ReportPortalClient.class);
 	private ReportPortalStepFormat format;
 
 	@BeforeEach
 	public void setupMock() {
-		TestUtils.mockLaunch(client, null, storyId, scenarioId, stepIds);
+		TestUtils.mockLaunch(client, null, storyId, scenarioId, exampleIds);
+		TestUtils.mockNestedSteps(client, stepIds);
 		TestUtils.mockBatchLogging(client);
 		format = new ReportPortalStepFormat(ReportPortal.create(client, TestUtils.standardParameters()));
 	}
 
 	@Test
 	public void verify_story_with_examples() {
-		TestUtils.run(format, "stories/Examples.story");
+		TestUtils.run(format, "stories/Examples.story", new StockExamplesSteps());
 
-
+		verify(client, times(1)).startTestItem(any());
+		verify(client, times(1)).startTestItem(same(storyId), any());
+		ArgumentCaptor<StartTestItemRQ> captor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+//		verify(client, times(2)).startTestItem(same(scenarioId), captor.capture());
 	}
 }
