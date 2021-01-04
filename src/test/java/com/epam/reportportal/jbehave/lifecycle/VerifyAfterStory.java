b90f89b49a98f57d5e26755ff4090a3100775634
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-01-04 14:13:23 +0300

    VerifyAfterStory test

--- src/test/java/com/epam/reportportal/jbehave/lifecycle/VerifyAfterStory.java
+++ src/test/java/com/epam/reportportal/jbehave/lifecycle/VerifyAfterStory.java
@@ -25,19 +25,18 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.*;
 
-public class VerifyBeforeStory extends BaseTest {
+public class VerifyAfterStory extends BaseTest {
 
 	private final String storyId = CommonUtils.namedId("story_");
-	private final String beforeStepId = CommonUtils.namedId("before_story_step_");
 	private final String scenarioId = CommonUtils.namedId("scenario_");
+	private final String afterStepId = CommonUtils.namedId("after_story_step_");
 	private final String stepId = CommonUtils.namedId("step_");
 
-	private final List<Pair<String, List<String>>> steps = Arrays.asList(Pair.of(beforeStepId, Collections.emptyList()),
-			Pair.of(scenarioId, Collections.singletonList(stepId))
+	private final List<Pair<String, List<String>>> steps = Arrays.asList(Pair.of(scenarioId, Collections.singletonList(stepId)),
+			Pair.of(afterStepId, Collections.emptyList())
 	);
 
 	private final ReportPortalClient client = mock(ReportPortalClient.class);
@@ -49,13 +48,13 @@ public class VerifyBeforeStory extends BaseTest {
 		mockBatchLogging(client);
 	}
 
-	private static final String STORY_PATH = "stories/lifecycle/BeforeStory.story";
+	private static final String STORY_PATH = "stories/lifecycle/AfterStory.story";
 	private static final String SCENARIO_NAME = "The scenario";
 	private static final String STEP_NAME = "Given I have empty step";
 	private static final String LIFECYCLE_STEP_NAME = "Then I have another empty step";
 
 	@Test
-	public void verify_before_story_lifecycle_step_reporting() {
+	public void verify_after_story_lifecycle_step_reporting() {
 		run(format, STORY_PATH, new EmptySteps());
 
 		verify(client).startTestItem(any());
@@ -65,16 +64,17 @@ public class VerifyBeforeStory extends BaseTest {
 
 		// Start items verification
 		List<StartTestItemRQ> startItems = startCaptor.getAllValues();
-		StartTestItemRQ beforeStoryStart = startItems.get(0);
-		assertThat(beforeStoryStart.getName(), equalTo(LIFECYCLE_STEP_NAME));
-		assertThat(beforeStoryStart.getCodeRef(), equalTo(STORY_PATH + String.format("/[STEP:%s]", LIFECYCLE_STEP_NAME)));
-		assertThat(beforeStoryStart.getType(), equalTo(ItemType.STEP.name()));
 
 		String scenarioCodeRef = STORY_PATH + String.format("/[SCENARIO:%s]", SCENARIO_NAME);
-		StartTestItemRQ scenarioStart = startItems.get(1);
+		StartTestItemRQ scenarioStart = startItems.get(0);
 		assertThat(scenarioStart.getName(), equalTo(SCENARIO_NAME));
 		assertThat(scenarioStart.getCodeRef(), equalTo(scenarioCodeRef));
 		assertThat(scenarioStart.getType(), equalTo(ItemType.SCENARIO.name()));
+
+		StartTestItemRQ beforeStoryStart = startItems.get(1);
+		assertThat(beforeStoryStart.getName(), equalTo(LIFECYCLE_STEP_NAME));
+		assertThat(beforeStoryStart.getCodeRef(), equalTo(STORY_PATH + String.format("/[STEP:%s]", LIFECYCLE_STEP_NAME)));
+		assertThat(beforeStoryStart.getType(), equalTo(ItemType.STEP.name()));
 
 		StartTestItemRQ step = startItems.get(2);
 		String stepCodeRef = scenarioCodeRef + String.format("/[STEP:%s]", STEP_NAME);
@@ -84,9 +84,9 @@ public class VerifyBeforeStory extends BaseTest {
 
 		// Finish items verification
 		ArgumentCaptor<FinishTestItemRQ> finishStepCaptor = ArgumentCaptor.forClass(FinishTestItemRQ.class);
-		verify(client).finishTestItem(same(beforeStepId), finishStepCaptor.capture());
 		verify(client).finishTestItem(same(stepId), finishStepCaptor.capture());
 		verify(client).finishTestItem(same(scenarioId), finishStepCaptor.capture());
+		verify(client).finishTestItem(same(afterStepId), finishStepCaptor.capture());
 		verify(client).finishTestItem(same(storyId), finishStepCaptor.capture());
 
 		List<FinishTestItemRQ> finishItems = finishStepCaptor.getAllValues();
